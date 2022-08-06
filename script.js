3ce7c26c84faf797f7750541de7db2c80550b948
IndarSetiawan <sgngsgrt@gmail.com>
2022-08-06 13:44:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const notif = document.querySelector(".notif");
+// const lope = document.querySelectorAll(".icon-lope");
+const lope1 = document.getElementById("lope-1");
+
+notif.addEventListener("click", function () {
+  console.log("hebat");
+});
+
+lope1.addEventListener("click", function () {
+  console.log("hebat");
+});
